34409911e8e9ed76b66937294c50d275c22b7d40
dcamelog <69636867+dcamelog@users.noreply.github.com>
2021-11-11 17:29:45 -0500

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -5,13 +5,35 @@ import CaminosMinimosP1 as minim
 import RecorridosP23 as rec
 import Fiesta as ft
 
-def cargarDatos():
+def cargarDatos(archivo:str):
     matriz = []
-    with open(os.path.dirname(__file__) + '/Data/pruebaFiesta5.txt', 'r') as file:
+    with open(os.path.dirname(__file__) + f'/Data/{archivo}.txt', 'r') as file:
         lineas = file.readlines()
         for linea in lineas:
             matriz.append(list(map(int, (linea.replace('\n','').split('\t')))))
         return matriz
+    
+def main():
+    vis=True
+    while vis==True: 
+        f=int (input("Seleccione el archivo que desea cargar (1:distances5, 2:distances100, 3:distances:1000, 4:Salir de la aplicacion)\n"))
+        matriz=0
+        if f==1:
+            matriz=cargarDatos("distances5")
+        elif f==2:
+            matriz=cargarDatos("distances100")
+        elif f==3:
+            matriz=cargarDatos("distances1000")
+        elif f==4:
+            vis=False
+        if f!=4:
+            print("matriz cargada correctamente")
+            print("Cargando algorimos de caminos minimo\nPuede tardar un poco")    
+            print(f"Warshall: {minim.warshall(matriz)}")
+            print(f"Bellman: {minim.bellman(matriz)}")
+    
 
+
+main()
 
 print(ft.fiesta(cargarDatos()))
